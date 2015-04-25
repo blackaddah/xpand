16813c910dca8702a727186f29ffef7eaec47d14
blackaddah <blackaddah@users.noreply.github.com>
2015-04-25 22:53:06 +0200

    Code cleanup

--- content.js
+++ content.js
@@ -1,21 +1,22 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == "loading") {
+        var newurl;
 
         // tumblr post image
         if (urlmatch = tab.url.match(/(^https?\:\/\/(?:gs1\.wac\.edgecastcdn\.net\/)?.*(?:media|data)\.tumblr\.com\/(?:.*\/)?tumblr_.*_)(250|400|500|540|1280)(\.(?:jpg|gif|png))$/i))
-            var newurl = get_tumblr_url(0, urlmatch);
+            newurl = get_tumblr_url(0, urlmatch);
 
         // tumblr profile pic
         else if (urlmatch = tab.url.match(/(^https?\:\/\/(?:[0-9]+)?.*\.media\.tumblr\.com\/avatar_.*_)(16|32|64|128|256|512)(\.(?:jpg|gif|png))$/i))
-            var newurl = get_tumblr_url(1, urlmatch);
+            newurl = get_tumblr_url(1, urlmatch);
 
         // deviantart thumbnail or preview image
         else if (urlmatch = tab.url.match(/(https?\:\/\/th[0-9]{2}\.deviantart\.net\/.*)\/(?:150|200H|PRE)(\/.*)$/i))
-            var newurl = get_deviantart_url(1, urlmatch);
+            newurl = get_deviantart_url(0, urlmatch);
 
         // another deviantart thumb, but weirdly crafted
         else if (urlmatch = tab.url.match(/(https?\:\/\/)(t[0-9]{2})(\.deviantart\.net)\/.*\b(?:filters:).*\/(pre[0-9]{2})(\/.*)$/i))
-            var newurl = get_deviantart_url(2, urlmatch);
+            newurl = get_deviantart_url(1, urlmatch);
 
         if (newurl) // if there is a bigger version of the image, load it
             chrome.tabs.update(tabId, {url: newurl});
@@ -23,11 +24,13 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 function get_tumblr_url(type, urlmatch) {
-    if (type == 0) // it's an img post
-        var sizes = [ 1280, 540, 500, 400, 250 ];
+    var sizes;
+
+    if (type === 0) // it's an img post
+        sizes = [ 1280, 540, 500, 400, 250 ];
 
     else if (type == 1) // it's a profile picture
-        var sizes = [ 512, 256, 128, 64, 32, 16 ];
+        sizes = [ 512, 256, 128, 64, 32, 16 ];
 
     var actual_size = urlmatch[2];
     var url_start = urlmatch[1];
@@ -48,11 +51,13 @@ function get_tumblr_url(type, urlmatch) {
 }
 
 function get_deviantart_url(type, urlmatch) {
-    if (type == 1) // usual thumbnail, we remove the size str from the url
-        var url = (urlmatch[1] + urlmatch[2]);
+    var url;
 
-    else if (type == 2) // the big, large, weird thumb; we gotta reorder params
-        var url = (urlmatch[1] + urlmatch[4] + urlmatch[3] + urlmatch[5]);
+    if (type === 0) // usual thumbnail, we remove the size str from the url
+        url = (urlmatch[1] + urlmatch[2]);
+
+    else if (type === 1) // the big, large, weird thumb; we gotta reorder params
+        url = (urlmatch[1] + urlmatch[4] + urlmatch[3] + urlmatch[5]);
 
     if (url_exists(url))
         return url;
